182d95b8d4b08398b38e49ca4df2d111f27d1db5
Dharshan-J <19euit041@skcet.ac.in>
2024-02-14 00:58:12 +0530

    list fetch and added card component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
+import SearchBar from './components/SearchBar';
+import RecipeDetail from './components/RecipeDetail';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+height: 100vh;`
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={<SearchBar  />}
+          />
+          <Route
+            path="/recipe/:id"
+            element={<RecipeDetail  />}
+          />
+
+        </Routes>
+      </Layout>
+    </Router>
+    </ChakraProvider>
   );
 }
 
